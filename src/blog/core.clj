d086ab7bff4d9a642c3fe3ba97c98ee272d80cd2
John Mwashuma <jmwashuma@live.com>
2017-09-05 16:42:41 +0300

    Add Http responce middleware

--- src/blog/core.clj
+++ src/blog/core.clj
@@ -1,14 +1,30 @@
 (ns blog.core
-  (:require [ring.adapter.jetty :as jetty]))
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.middleware.params :refer [wrap-params]]))
 
 (defn myapp [request]
   "This is the entry point"
-  [request]
-  {:body "Hello Clojure"
-   :status 200
-   :header {"Content-Type" "text/html"}})
+  (str "Hello, " (get (:params request) "name"))) ;; Gets a name from a http request
+
+(defn string-response-middleware
+  "Accepts a request and returns a responce"
+  [handler]
+  (fn [request]
+    (let [responce (handler request)]
+    (if (instance? String responce)
+    {:body responce
+    :status 200
+    :header {"Content-Type" "text/html"}}
+    responce)))
+  )
+
+;; Ring midddle ware
+(def handler
+  (-> myapp
+      string-response-middleware
+      wrap-params))
 
 (defn -main []
-  (jetty/run-jetty myapp {:port 3000}))
+  (jetty/run-jetty handler {:port 3000}))
 
 
