873d9e12d78fbacc27ad9f3b356a9d7484220c09
John Mwashuma <jmwashuma@live.com>
2017-09-08 17:27:18 +0300

    Use jdbc to pull records from postgres db

--- src/dreams_bucket/core.clj
+++ src/dreams_bucket/core.clj
@@ -1,20 +1,35 @@
 (ns dreams_bucket.core
   (:require [ring.adapter.jetty :as jetty]
+            [dreams_bucket.db_connect :as db]
+            [clojure.java.jdbc :as jdbc]
             [compojure.core :refer [defroutes GET POST]]
             [compojure.route :refer [resources]]
             [net.cgrand.enlive-html :as enlive]))
 
 
+;; Get all bucket activities
+(defn get_bucket_activities []
+(jdbc/with-connection db/postgresql-db
+  (jdbc/with-query-results results
+    ["SELECT * FROM tbl_bucket_activity"]
+    (into [] results))))
+
 ;; Define the templates
 (enlive/deftemplate all_wishes_page "templates/all_wishes.html"
   []
   [:head :title] (enlive/content "Dreams Bucket | Home")
-  [:body :div#side-menu :h1] (enlive/content "Dreams Bucket"))
+  [:div#side-menu :h1] (enlive/content "Dreams Bucket")
+  [:b#bucket_title] (enlive/clone-for [s (map :bucket_activity_name (get_bucket_activities))](enlive/content s))
+  [:div#bucket_name :button] (enlive/clone-for [s (map :bucket_name (get_bucket_activities))](enlive/content s))
+  [:div#bucket_description :p] (enlive/clone-for [s (map :bucket_activity_description (get_bucket_activities))](enlive/content s)))
 
 (enlive/deftemplate my_wishes_page "templates/my_wishes.html"
   []
   [:head :title] (enlive/content "Dreams Bucket | My Wishes")
-  [:body :div#side-menu :h1] (enlive/content "Dreams Bucket"))
+  [:body :div#side-menu :h1] (enlive/content "Dreams Bucket")
+  [:b#bucket_title] (enlive/clone-for [s (map :bucket_activity_name (get_bucket_activities))](enlive/content s))
+  [:div#bucket_name :button] (enlive/clone-for [s (map :bucket_name (get_bucket_activities))](enlive/content s))
+  [:div#bucket_description :p] (enlive/clone-for [s (map :bucket_activity_description (get_bucket_activities))](enlive/content s)))
 
 (enlive/deftemplate manage_account_page "templates/manage_account.html"
   []
@@ -24,12 +39,18 @@
 (enlive/deftemplate private_wishes_page "templates/private_wishes.html"
   []
   [:head :title] (enlive/content "Dreams Bucket | Private Wishes")
-  [:body :div#side-menu :h1] (enlive/content "Dreams Bucket"))
+  [:body :div#side-menu :h1] (enlive/content "Dreams Bucket")
+  [:b#bucket_title] (enlive/clone-for [s (map :bucket_activity_name (get_bucket_activities))](enlive/content s))
+  [:div#bucket_name :button] (enlive/clone-for [s (map :bucket_name (get_bucket_activities))](enlive/content s))
+  [:div#bucket_description :p] (enlive/clone-for [s (map :bucket_activity_description (get_bucket_activities))](enlive/content s)))
 
 (enlive/deftemplate public_wishes_page "templates/public_wishes.html"
   []
   [:head :title] (enlive/content "Dreams Bucket | Public Wishes")
-  [:body :div#side-menu :h1] (enlive/content "Dreams Bucket"))
+  [:body :div#side-menu :h1] (enlive/content "Dreams Bucket")
+  [:b#bucket_title] (enlive/clone-for [s (map :bucket_activity_name (get_bucket_activities))](enlive/content s))
+  [:div#bucket_name :button] (enlive/clone-for [s (map :bucket_name (get_bucket_activities))](enlive/content s))
+  [:div#bucket_description :p] (enlive/clone-for [s (map :bucket_activity_description (get_bucket_activities))](enlive/content s)))
 
 (enlive/deftemplate wish_form_page "templates/wish_form.html"
   []
